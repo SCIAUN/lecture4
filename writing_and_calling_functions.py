84a15b5b07a0f2a2074a412044ae57560e61025b
mohammad hossain <you@example.com>
2018-04-17 12:02:27 +0430

    writing and invoking a function

---
+++ writing_and_calling_functions.py
@@ -0,0 +1,8 @@
+def is_even(number):
+    """this code with check whether the number is even
+    or odd
+    """
+    print("inside is_even function")
+    return number%2==0
+print(is_even(5))
+print(is_even(10))
